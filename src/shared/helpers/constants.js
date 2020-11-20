5cf847ac848953c1368ad50e39270a42205b9ca6
Mevan <mevanabeydeera@gmail.com>
2020-11-21 01:37:43 +0400

    ADD: Helper files

---
+++ src/shared/helpers/constants.js
@@ -0,0 +1,70 @@
+export const COINS = [
+  {
+    name: 'BTC',
+    binanceSymbol: 'btcusd',
+    tvSymbol: 'BINANCE:BTCUSDT'
+  },
+  {
+    name: 'ETH',
+    binanceSymbol: 'ethusd',
+    tvSymbol: 'BINANCE:ETHUSDT'
+  },
+  {
+    name: 'LTC',
+    binanceSymbol: 'ltcbtc',
+    tvSymbol: 'BINANCE:LTCBTC'
+  },
+  {
+    name: 'UNI',
+    binanceSymbol: 'unibtc',
+    tvSymbol: 'BINANCE:UNIBTC'
+  },
+  {
+    name: 'ADA',
+    binanceSymbol: 'adabtc',
+    tvSymbol: 'BINANCE:ADABTC'
+  },
+  {
+    name: 'AAVE',
+    binanceSymbol: 'aavebtc',
+    tvSymbol: 'BINANCE:AAVEBTC'
+  },
+  {
+    name: 'DOT',
+    binanceSymbol: 'dotbtc',
+    tvSymbol: 'BINANCE:DOTBTC'
+  },
+  {
+    name: 'RUNE',
+    binanceSymbol: 'runebtc',
+    tvSymbol: 'BINANCE:RUNEBTC'
+  },
+  {
+    name: 'LINK',
+    binanceSymbol: 'linkbtc',
+    tvSymbol: 'BINANCE:LINKBTC'
+  },
+];
+
+const currencies = [
+  {
+    name: 'USD',
+    code: 'USD',
+  },
+  {
+    name: 'LKR',
+    code: 'LKR',
+  },
+  {
+    name: 'EUR',
+    code: 'EUR',
+  },
+  {
+    name: 'GBP',
+    code: 'GBP',
+  },
+  {
+    name: 'AUD',
+    code: 'AUD',
+  },
+];
